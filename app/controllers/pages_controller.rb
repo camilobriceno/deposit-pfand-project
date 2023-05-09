a982fa89984884b4af089ccd9184f7c0056266a7
Alam Sena <alam@koarquitetos.com>
2023-05-09 20:10:49 +0200

    custom routs, redirecting

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,18 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :deposit]
 
   def home
+
   end
 
+  def zipcode
+    raise
+  end
+
+
+  def deposit
+
+  end
 
   def index
     @bottles = Bottle.all
